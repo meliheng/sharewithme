59a3fb544f1a1c063031a460b27829a99959cadb
meliheng <69640069+meliheng@users.noreply.github.com>
2023-12-28 22:50:57 +0300

    Create login with google button

---
+++ lib/auth/presentation/widget/google_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:sharewithme/export.dart';
+import 'package:sharewithme/shared/constants/style_constants.dart';
+
+class GoogleButton extends StatelessWidget {
+  final void Function()? onTap;
+  const GoogleButton({
+    Key? key,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: ColorConstants.grayV2,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              ImageC.googleIcon,
+              height: 20,
+            ),
+            const SizedBox(width: 20),
+            Text(
+              AuthStringConstants.loginWithGoogle,
+              textAlign: TextAlign.center,
+              style: StyleContants.black14W5,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
