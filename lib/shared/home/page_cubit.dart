bb76aca4005bd57f142135989a8a16063925d92d
meliheng <69640069+meliheng@users.noreply.github.com>
2023-12-31 00:14:01 +0300

    Update page_cubit.dart

--- lib/shared/home/page_cubit.dart
+++ lib/shared/home/page_cubit.dart
@@ -1,9 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:sharewithme/activity/presentation/activity_screen.dart';
-import 'package:sharewithme/auth/application/_application_exporter.dart';
-import 'package:sharewithme/shared/constants/color_constants.dart';
-import 'package:sharewithme/shared/home/page_state.dart';
+import 'package:sharewithme/export.dart';
 
 class PageCubit extends Cubit<PageState> {
   PageCubit() : super(PageState.initial());
@@ -13,6 +10,20 @@ class PageCubit extends Cubit<PageState> {
   }
 
   Widget get getCurrentScreen => screens[state.currentIndex];
+  Widget? showFloatActionButton(BuildContext context) {
+    if (state.currentIndex == 0) {
+      return FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          ActivityListCubit cubit = ActivityListCubit();
+          cubit.addActivity(context);
+        },
+      );
+    } else {
+      return null;
+    }
+  }
+
   Color? iconColor(int index) =>
       index == state.currentIndex ? ColorConstants.greenV1 : null;
   List<Widget> get screens => [
